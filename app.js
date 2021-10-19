c6ca3e849a5fca59f1b9871037fdda915b58002d
Jerum Hubbert <hubbertj@gmail.com>
2021-10-19 03:34:43 -0700

    adding more to the mix

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-const elasticsearch = require('elasticsearch');
-const client = new elasticsearch.Client({
+const { elasticsearch } = require('elasticsearch');
+const { Client } = require('pg');
+
+const pgClient = new Client()
+const elasticSearchClient = new elasticsearch.Client({
     hosts: ['https://username:password@host:port']
 });
+
+/**
+ * main script function
+ */
+const main = () => {
+    // read indexs 
+
+    // loop indexs and call converters | mappers | injectors
+
+}
+
+main();
+
+
+
